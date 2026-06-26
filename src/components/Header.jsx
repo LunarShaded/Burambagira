fbfd91a3487a827adb159f99dd8ea9f97fe68dad
Maksym Shevchyk <maksim.shevchik@gmail.com>
2026-06-26 16:49:44 +0200

    New component 'Header'

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import styles from './Header.module.css'
+
+
+export default function Header(){
+    return(
+        <>
+          <section className={styles.headerSection}>
+            <div className={styles.headerTitle}>
+                  <h3>Burambagira</h3>
+                  <p>High School</p>
+            </div>
+
+         <nav>
+
+           <div className={styles.nav}>
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>About Us</a></li>
+                    <li><a href='#'>Contact Us</a></li>
+                </ul>
+           </div>
+
+        </nav>
+          </section>
+        </>
+    )
+}
